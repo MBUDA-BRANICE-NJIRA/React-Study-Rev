beb33de33371c2fc9a7efb0a24810346c4d3e45b
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-11 22:26:22 +0300

    Learnt on arrow function

--- src/components/greet.js
+++ src/components/greet.js
@@ -1,8 +1,11 @@
 import React from "react";
-const Greet = () => {
-    return ( 
-        <h1>Hello Branice</h1>
-     );
-}
+// const Greet = () => {
+//     return ( 
+//         <h1>Hello Branice</h1>
+//      );
+// }
  
-export default Greet;
+// export default Greet;
+
+const Greet = () => <h1>Helloo Branice</h1>
+export default Greet;  //Short cut  Arrow Function
---
+++ src/components/welcome.js
@@ -0,0 +1,8 @@
+import React,{ React} from 'react';
+
+class welcome extends Component{
+    render(){
+        return <h1>Welcome home</h1>
+    }
+}
+export default welcome;
