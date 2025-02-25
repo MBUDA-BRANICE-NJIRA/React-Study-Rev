ac74299cfbdf6b09628bd6e7fa1307f60df94d42
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-25 19:02:29 +0300

    Deleted unuseful components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/greet';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-    
+     
     </div>
   );
 }
--- src/components/welcome.js
+++
@@ -1,8 +0,0 @@
-import React,{ React} from 'react';
-
-class welcome extends Component{
-    render(){
-        return <h1>Welcome home</h1>
-    }
-}
-export default welcome;
